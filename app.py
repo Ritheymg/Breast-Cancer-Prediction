3a1e07c19481deabe7c72c99b86539ec32db4509
Ritheymg <136904061+Ritheymg@users.noreply.github.com>
2025-09-24 16:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,8 +127,8 @@ def get_radar_chart(input_data):
 
 
 def add_predictions(input_data):
-    model = pickle.load(open("model/model.pkl", "rb"))
-    scaler = pickle.load(open("model/scaler.pkl", "rb"))
+    model = pickle.load(open("model.pkl", "rb"))
+    scaler = pickle.load(open("scaler.pkl", "rb"))
     
     input_array = np.array(list(input_data.values())).reshape(1, -1)
     input_array_scaled = scaler.transform(input_array)
@@ -174,3 +174,4 @@ if __name__ == '__main__':
     main()
     
     
+
